f3863f5b1c8a52cc2618c52d72923bc05c4d59c0
Andrei Dimaano <andreiidimaano@gmail.com>
2021-08-14 21:07:32 -0700

    navbar - signin sign up , logo, responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,32 +8,17 @@ import {
   Code,
   Grid,
   theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+} from "@chakra-ui/react";
+import { Logo } from "./Logo";
+import NavbarSignUp from "./navbar/navbar";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+      <Box height="100vh">
+        <NavbarSignUp />
+        <ColorModeSwitcher pos="absolute" bottom="8" right="8" />
       </Box>
     </ChakraProvider>
   );
