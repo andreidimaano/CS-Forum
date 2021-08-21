6104e74a89a22e3a65c58570b09c7e8e60941efa
Andrei Dimaano <andreiidimaano@gmail.com>
2021-08-20 21:18:18 -0700

    center navbar items

--- client/src/navbar/navbar.js
+++ client/src/navbar/navbar.js
@@ -27,13 +27,15 @@ const NavbarSignUp = () => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box py={"8px"}>
-      <Flex 
+    <Box 
+      borderBottom={1}
+      borderStyle={"solid"}
+      py={"8px"}
+      borderColor={useColorModeValue("gray.200", "gray.900")}
+    >
+      <Flex
         bg={useColorModeValue("white", "gray.800")}
         color={useColorModeValue("gray.600", "white")}
-        borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
         px={"16px"}
         align={"center"}
         ml={"auto"}
@@ -41,8 +43,9 @@ const NavbarSignUp = () => {
         maxW={"1280px"}
       >
         <Flex
-          flex={{ base: 1, md: "auto" }}
+          flex={{ base: "1", md: "auto" }}
           ml={{ base: -2 }}
+          mr={{ base: "auto"}}
           display={{ base: "flex", md: "none" }}
         >
           <IconButton
@@ -54,7 +57,7 @@ const NavbarSignUp = () => {
             aria-label={"Toggle Navigation"}
           />
         </Flex>
-        <Flex align={"center"}justify={{ base: "center"}}>
+        <Flex alignSelf={{base: "center"}} align={"center"} justify={{ base: "center" }}>
           <Text
             textAlign={useBreakpointValue({ base: "center", md: "left" })}
             fontFamily={"heading"}
@@ -65,13 +68,13 @@ const NavbarSignUp = () => {
           </Text>
         </Flex>
         <Flex flex={"auto"} align={"center"} justify={"space-between"}>
-          <Flex py={"16px"} display={{ base: 'none', md: 'flex' }} ml={10}>
+          <Flex py={"16px"} display={{ base: "none", md: "flex" }} ml={10}>
             Posts
-            {/* <DesktopNav /> */}
-          </Flex> 
+          </Flex>
           <Stack
             flex={{ base: 1, md: 0 }}
             justify={"flex-end"}
+            align={"center"}
             direction={"row"}
             spacing={6}
           >
@@ -106,7 +109,8 @@ const NavbarSignUp = () => {
 
 export default NavbarSignUp;
 
-{/* <Flex
+{
+  /* <Flex
 bg={useColorModeValue("white", "gray.800")}
 color={useColorModeValue("gray.600", "white")}
 borderBottom={1}
@@ -157,4 +161,5 @@ ml={"auto"}
     Create Account
   </Button>
 </Stack>
-</Flex> */}
+</Flex> */
+}
