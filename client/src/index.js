733a9f4ef0667594734998f54c5b6a34409c45a1
alfredzhuang <alfredzhuang123@gmail.com>
2021-09-02 17:32:52 -0700

    use Apollo Client to render App

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,9 @@
-import { ColorModeScript } from "@chakra-ui/react";
-import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import ApolloProvider from "./ApolloProvider";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
